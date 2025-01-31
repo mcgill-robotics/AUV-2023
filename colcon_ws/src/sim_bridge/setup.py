79814fc6b35f591d1baff1985450fa770ff630de
Alb33rt <albert931023@gmail.com>
2025-01-30 19:12:27 -0500

    Debug package.xml and setup.py files

--- colcon_ws/src/sim_bridge/setup.py
+++ colcon_ws/src/sim_bridge/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'sim_bridge'
 
@@ -9,7 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
